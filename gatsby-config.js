b011b218b72da9163751cdccae1e9292ce6ccea9
Paul Duszak <duszak.paul@gmail.com>
2023-01-22 18:21:07 -0600

    Update analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,14 +123,9 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ["G-FGK793TDRR"],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
+        trackingIds: [
+          "G-FGK793TDRR", // Google Analytics / GA
+        ],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
